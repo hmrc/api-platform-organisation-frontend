42be9ee800f079fbb5ab360b3d4f691014f6d63d
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2025-01-28 11:19:46 +0000

    APIS-7446 Add submission structure framework

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -16,8 +16,8 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 80,
-    ScoverageKeys.coverageMinimumBranchTotal := 70,
+    ScoverageKeys.coverageMinimumStmtTotal := 85,
+    ScoverageKeys.coverageMinimumBranchTotal := 80,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
