8fb2c6ae81e6f4bedc7eddac5be57caf7147fdf0
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2026-01-06 14:06:51 +0000

    PR-Bot Tidying

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.19.0"
+  private val bootstrapVersion = "10.5.0"
   private val tpdDomainVersion = "0.14.0"
   private val orgDomainVersion = "0.11.0"
-  private val appDomainVersion = "0.87.0"
+  private val appDomainVersion = "0.95.0"
 
   val compile = Seq(
     "uk.gov.hmrc"      %% "bootstrap-frontend-play-30"       % bootstrapVersion,
-    "uk.gov.hmrc"      %% "play-frontend-hmrc-play-30"       % "12.13.0",
+    "uk.gov.hmrc"      %% "play-frontend-hmrc-play-30"       % "12.25.0",
     "uk.gov.hmrc"      %% "api-platform-tpd-domain"          % tpdDomainVersion,
     "uk.gov.hmrc"      %% "http-metrics"                     % "2.9.0",
     "uk.gov.hmrc"      %% "api-platform-organisation-domain" % orgDomainVersion,
@@ -23,7 +23,7 @@ object AppDependencies {
     "org.jsoup"    % "jsoup"                                     % "1.18.3"         % Test,
     "uk.gov.hmrc" %% "api-platform-organisation-domain-fixtures" % orgDomainVersion % Test,
     "uk.gov.hmrc" %% "api-platform-test-tpd-domain"              % tpdDomainVersion % Test,
-    "uk.gov.hmrc" %% "api-platform-application-domain-fixtures"  % appDomainVersion
+    "uk.gov.hmrc" %% "api-platform-application-domain-fixtures"  % appDomainVersion % Test
   )
 
   val it = Seq.empty
