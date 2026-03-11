cc6c9adfa181a6efaa7ff76f41f317db7eaec4f1
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2026-03-11 12:51:31 +0000

    APIS-7982 Update check list page (#51)
    * APIS-7982 Update check list page
    
    * APIS-7982 Updated checklist page
    
    * APIS-7982 PR bot actions
    
    * APIS-7982 Updated checklist page

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,10 +2,10 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.6.0"
+  private val bootstrapVersion = "10.7.0"
   private val tpdDomainVersion = "0.14.0"
-  private val orgDomainVersion = "0.13.0"
-  private val appDomainVersion = "0.95.0"
+  private val orgDomainVersion = "0.14.0"
+  private val appDomainVersion = "0.96.0"
 
   val compile = Seq(
     "uk.gov.hmrc"      %% "bootstrap-frontend-play-30"       % bootstrapVersion,
