be60c082c43e94413843328d3cd39dc083a251b1
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2026-09-07 16:03:36 +0100

    APIS-8323 PR bot action

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"      %% "bootstrap-frontend-play-30"       % bootstrapVersion,
-    "uk.gov.hmrc"      %% "play-frontend-hmrc-play-30"       % "13.11.0",
+    "uk.gov.hmrc"      %% "play-frontend-hmrc-play-30"       % "13.12.0",
     "uk.gov.hmrc"      %% "api-platform-common-domain"       % commonDomainVersion,
     "uk.gov.hmrc"      %% "api-platform-tpd-domain"          % tpdDomainVersion,
     "uk.gov.hmrc"      %% "api-platform-organisation-domain" % orgDomainVersion,
